7b2bc183c4f9277ccb1775c0fa57185b3d7761bd
Justin Stoddard <justinst@usa-it-b8bg8wp.corp.unicity.net>
2018-08-29 14:47:23 -0600

    Added more content

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { Container, Segment, Divider, Header } from 'semantic-ui-react';
+import { Container, Segment, Divider, Header, Grid } from 'semantic-ui-react';
 
 const About = () => {
   return(
     <Container>
       <Divider hidden/>
-      <Segment textAlign="center">
+      <Segment textAlign="center" inverted>
         <Header as="h1">About</Header>
       </Segment>
+      <Divider hidden/>
+      <Segment textAlign="center" inverted>
+        <Header as="h4">Education</Header>
+      </Segment>
+      <Divider hidden/>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Segment inverted>
+              <Segment textAlign="center">
+                <Header as="h5">The University Of Utah</Header>
+              </Segment>
+              <Segment>
+                Content
+              </Segment>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Segment inverted>
+              <Segment textAlign="center">
+                <Header as="h5">Salt Lake Community College</Header>
+              </Segment>
+              <Segment>
+                Content
+              </Segment>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Segment inverted>
+              <Segment textAlign="center">
+                <Header as="h5">Udemy.com</Header>
+              </Segment>
+              <Segment>
+                Content
+              </Segment>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Segment inverted>
+              <Segment textAlign="center">
+                <Header as="h5">Treehouse.com</Header>
+              </Segment>
+              <Segment>
+                Content
+              </Segment>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Container>
   )
 }
