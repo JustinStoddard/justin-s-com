e4f0231da22e48810de495c95475660c8e77f621
Justin Stoddard <justinst@usa-it-b8bg8wp.corp.unicity.net>
2018-09-19 13:15:48 -0600

    Fixing spacing issues

--- src/components/mobile/mobileDemos.js
+++ src/components/mobile/mobileDemos.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Loader from '../../Loader';
 import { Segment, Container, Divider, Header, Button, List } from 'semantic-ui-react';
 import Loadable from 'react-loadable';
@@ -27,28 +27,31 @@ class Demos extends Component {
               <VideoDemoLoader />
             </Segment>
           :
-            <Segment inverted>
-              <Segment color="blue" textAlign="center">
-                <Header as="h2">YouTube Player</Header>
-              </Segment>
-              <Segment textAlign="left" color="blue">
-                <strong>This</strong> demos show cases the ability to use YouTubes API to search for, and view videos. There is also a lot of work behind the scenes with State, Props, and Conditional Rendering. <strong>Also star the repo <a href="https://github.com/JustinStoddard/JustinS.com">here</a></strong>
-              </Segment>
-              <Segment color="blue">
-                <Segment textAlign="center" inverted>
-                  <Header as="h3">TECH</Header>
+            <Fragment>
+              <Segment inverted>
+                <Segment color="blue" textAlign="center">
+                  <Header as="h2">YouTube Player</Header>
+                </Segment>
+                <Segment textAlign="left" color="blue">
+                  <strong>This</strong> demos show cases the ability to use YouTubes API to search for, and view videos. There is also a lot of work behind the scenes with State, Props, and Conditional Rendering. <strong>Also star the repo <a href="https://github.com/JustinStoddard/JustinS.com">here</a></strong>
                 </Segment>
                 <Segment color="blue">
-                  <List as="ul">
-                    <List.Item as="li">React Js</List.Item>
-                    <List.Item as="li">Semantic-UI</List.Item>
-                    <List.Item as="li">YouTube API</List.Item>
-                    <List.Item as="li">NPM Lodash</List.Item>
-                  </List>
+                  <Segment textAlign="center" inverted>
+                    <Header as="h3">TECH</Header>
+                  </Segment>
+                  <Segment color="blue">
+                    <List as="ul">
+                      <List.Item as="li">React Js</List.Item>
+                      <List.Item as="li">Semantic-UI</List.Item>
+                      <List.Item as="li">YouTube API</List.Item>
+                      <List.Item as="li">NPM Lodash</List.Item>
+                    </List>
+                  </Segment>
                 </Segment>
+                <Button color="blue" size="huge" onClick={() => this.setState({ videoDemo: !videoDemo })}>View Demo</Button>
               </Segment>
-              <Button color="blue" size="huge" onClick={() => this.setState({ videoDemo: !videoDemo })}>View Demo</Button>
-            </Segment>
+              <Divider hidden/>
+            </Fragment>
         }
       </Container>
     )
