c80827a45fb78a681125edd80a2eeca432c05265
Justin Stoddard <justinst@USA-IT-B8BG8WP.local>
2019-01-02 10:50:42 -0700

    pokeDex works

--- src/components/web/FunWithApisDemo/FunApisDemoIndex.js
+++ src/components/web/FunWithApisDemo/FunApisDemoIndex.js
@@ -1,49 +1,62 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
-import { Header, Segment, Icon, Button } from 'semantic-ui-react';
+import { Header, Segment, Icon, Button, Image, Grid } from 'semantic-ui-react';
 
 class ApiFunDemo extends Component {
   state = {
     allPokeMon: [],
     pokeMon: [],
+    pokeImage: null,
+    pokeMonNumber: 0,
   }
 
   componentDidMount() {
-    this.getAllPokeMon();
-  }
-
-  getAllPokeMon = async () => {
     const apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20";
-    await axios.get(apiUrl)
+    axios.get(apiUrl)
       .then(response => {
-        const manyPokeMon = response.data.results.slice(0,10);
+        const manyPokeMon = response.data.results.slice(0,20);
         this.setState({ allPokeMon: manyPokeMon });
         console.log("All Poke", manyPokeMon)
       }).catch(err => console.log(err))
   }
 
-  getSinglePokeMon = async () => {
-    const { allPokeMon } = this.state
-    console.log(allPokeMon)
-    let randomNumber = Math.floor((Math.random() * 10) + 1) //Generates a number between 1 & 10
-    const pokeMonUrl = allPokeMon !== null ? allPokeMon[randomNumber].url : console.log("Variable 'allPokeMon' is null") //Checks if allPokeMon does not equal null
-    console.log(pokeMonUrl)
-    if (pokeMonUrl === null ) { //Re-runs this call if "pokeMonUrl" is null
+  getSinglePokeMon = () => {
+    const { allPokeMon, pokeMonNumber } = this.state
+    if (allPokeMon[pokeMonNumber].url !== null ) { //Re-runs this call if "pokeMonUrl" is null
+      const pokeMonUrl = allPokeMon[pokeMonNumber].url; //Checks if allPokeMon does not equal null
+      axios.get(pokeMonUrl)
+        .then(response => {
+          const pokeMon = response.data;
+          const pngImage = pokeMon.sprites.front_default;
+          this.setState({ pokeMon: pokeMon, pokeImage: pngImage })
+          console.log(pokeMonNumber)
+          console.log(pokeMon.name)
+        }).catch(err => console.log(err))
+    } else {
       this.getSinglePokeMon
     }
-    await axios.get(pokeMonUrl)
-      .then(response => {
-        const pokeMon = response.data;
-        this.setState({ pokeMon: pokeMon })
-        console.log(randomNumber)
-        console.log(pokeMon.name)
-        console.log(pokeMon)
-      }).catch(err => console.log(err))
+  }
+
+  plusNumber = async () => {
+    const { pokeMonNumber } = this.state;
+    console.log(pokeMonNumber)
+    await this.setState({ pokeMonNumber: pokeMonNumber + 1 })
+    this.getSinglePokeMon()
+  }
+
+  minusNumber = async () => {
+    const { pokeMonNumber } = this.state;
+    console.log(pokeMonNumber)
+    if (pokeMonNumber === 0) {
+      await this.setState({ pokeMonNumber: pokeMonNumber - 0 })
+    } else {
+      await this.setState({ pokeMonNumber: pokeMonNumber - 1 })
+    }
+    this.getSinglePokeMon()
   }
 
   render() {
-    const { pokeMon, allPokeMon } = this.state
-    // console.log(pokeMon[number])
+    const { pokeMon, pokeImage } = this.state
     return(
       <Fragment>
         <Segment inverted>
@@ -52,14 +65,45 @@ class ApiFunDemo extends Component {
             <Icon loading name="spinner"/>
           </Segment>
         </Segment>
-        <Segment>
-          <Header as="h3">Name - {pokeMon.name}</Header>
-          <Header as="h3">Base XP - {pokeMon.base_experience}</Header>
-          <Header as="h3">Height - {pokeMon.height}</Header>
-          <Header as="h3">Weight - {pokeMon.weight}</Header>
-        </Segment>
-        <Button onClick={this.getAllPokeMon}>All Pokemon</Button>
-        <Button onClick={this.getSinglePokeMon}>One Pokemon</Button>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={6}></Grid.Column>
+              <Grid.Column width={4}>
+                <Segment inverted>
+                  <Segment textAlign="center">
+                    <Button onClick={this.minusNumber} size="large" color="red"><Icon fitted name="arrow left" size="large"/></Button>
+                    <Button onClick={this.plusNumber} size="large" color="red"><Icon fitted name="arrow right" size="large"/></Button>
+                  </Segment>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={6}></Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+              <Grid.Column width={6}></Grid.Column>
+              <Grid.Column width={4}>
+              <Segment inverted>
+                <Segment>
+                  {pokeMon !== [] ?
+                      <Fragment>
+                        <Segment textAlign="center" inverted>
+                          {pokeImage ? <Image src={pokeImage} size="large"/> : <Icon loading name="spinner"/>}
+                        </Segment>
+                        <Header as="h3">Name - {pokeMon.name}</Header>
+                        <Header as="h5">Base XP - {pokeMon.base_experience}</Header>
+                        <Header as="h5">Height - {pokeMon.height}</Header>
+                        <Header as="h5">Weight - {pokeMon.weight}</Header>
+                      </Fragment>
+                    :
+                    <Header as="h3">Push The Forward Button</Header>
+                  }
+                </Segment>
+              </Segment>
+              </Grid.Column>
+              <Grid.Column width={6}></Grid.Column>
+            </Grid.Row>
+          </Grid>
+        {/* <Button onClick={this.getSinglePokeMon}>PokeMon</Button> */}
       </Fragment>
     )
   }
