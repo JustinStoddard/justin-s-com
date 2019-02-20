21621ba0e49e19b67320864c6d152dd63030f8ce
Justin Stoddard <Stoddard.j.t@gmail.com>
2019-02-19 22:23:05 -0700

    worked on ui for content editor

--- src/components/web/Demos/ContentEditorDemo/ContentEditorIndex.js
+++ src/components/web/Demos/ContentEditorDemo/ContentEditorIndex.js
@@ -1,12 +1,57 @@
-import React, { Fragment } from 'react';
-import { Header, Segment, Icon } from 'semantic-ui-react';
+import React, { Fragment, useState } from 'react';
+import { Header, Segment, Icon, Grid, Container } from 'semantic-ui-react';
+
+
 
 const ContentEditorIndex = () => {
   return (
     <Fragment>
-      <Segment textAlign="center">
-        <Header as="h1">Not Broken</Header>
+      <Segment textAlign="center" color="yellow" inverted>
+        <Header as="h1">Under Contruction</Header>
+        <Icon name="warning sign" size="large"/>
       </Segment>
+      {/* The code for this Demo should go below here */}
+      <Container>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={5}>
+              <Segment>
+                Tabs
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={5}></Grid.Column>
+            <Grid.Column width={5}>
+              <Segment>
+                Country Selector
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={15}>
+              <Segment>
+                Main Banner
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={5}>
+              <Segment>
+                First Column
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={5}>
+              <Segment>
+                Second Column
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={5}>
+              <Segment>
+                Third Column
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     </Fragment>
   )
 }
