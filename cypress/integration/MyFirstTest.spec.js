4282149f64cbf85fa4838ad06db216810ffc2dca
Justin Stoddard <justinst@USA-IT-B8BG8WP.corp.unicity.net>
2019-05-09 13:44:37 -0600

    Added cypress tests

--- cypress/integration/MyFirstTest.spec.js
+++ cypress/integration/MyFirstTest.spec.js
@@ -1,13 +1,21 @@
 describe('My First Test', () => {
   it('Finds the content "type"', () => {
 
-    cy.visit('https://www.thisjustin.tech/') //I can go to any site and test UI but you shouldn't becauase that UI can cahnge at any time, so you should only test sites you control.
+    cy.visit('https://www.thisjustin.tech/Resume') //I can go to any site and test UI but you shouldn't becauase that UI can cahnge at any time, so you should only test sites you control.
 
-    cy.contains('Demos').click()
+    cy.get('.godfather-container', { timeout: 5000 }).contains('Experience').click()
+    cy.wait(3000)
+    cy.get('.godfather-container', { timeout: 5000 }).contains('Education').click()
+    cy.wait(3000)
+    cy.get('.godfather-container', { timeout: 5000 }).contains('Projects').click()
 
-    cy.contains('View Demo').click()
+    cy.wait(3000)
 
-    cy.contains('OPEN').click()
+    cy.get('.godfather-container', { timeout: 5000 }).contains('Experience').click()
+    cy.wait(3000)
+    cy.get('.godfather-container', { timeout: 5000 }).contains('Education').click()
+    cy.wait(3000)
+    cy.get('.godfather-container', { timeout: 5000 }).contains('Projects').click()
   })
 })
 
